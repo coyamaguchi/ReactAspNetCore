eda53ef41a543545c28075c2c2534bb2dc98f09f
coyamaguchi <coy@osite.com.br>
2022-09-25 19:14:21 -0300

    2022 09 25 - tudo ok

--- back/src/ProAtividade.API/Program.cs
+++ back/src/ProAtividade.API/Program.cs
@@ -1,10 +1,25 @@
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
-using ProAtividade.API.Data;
+using ProAtividade.Data.Context;
+using ProAtividade.Data.Repositories;
+using ProAtividade.Domain.Interfaces.Repositories;
+using ProAtividade.Domain.Interfaces.Services;
+using ProAtividade.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("Default"))
+);
+
+builder.Services.AddScoped<IAtividadeRepo, AtividadeRepo>();
+builder.Services.AddScoped<IGeralRepo, GeralRepo>();
+builder.Services.AddScoped<IAtividadeService, AtividadeService>();
 
 builder.Services.AddControllers()
                 .AddJsonOptions(options =>
@@ -12,12 +27,7 @@ builder.Services.AddControllers()
                      options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
                     }
                 );
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(
-    options => options.UseSqlite(builder.Configuration.GetConnectionString("Default"))
-);
+
 builder.Services.AddCors();
 
 var app = builder.Build();
