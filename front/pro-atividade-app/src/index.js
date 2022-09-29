69783c0ebe1d33f3cab079d995db1a9e8045b63c
coyamaguchi <coy@osite.com.br>
2022-09-29 11:25:49 -0300

    2022 09 29 - Finalizado

--- front/pro-atividade-app/src/index.js
+++ front/pro-atividade-app/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import { Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+import Menu from './components/Menu'
+import 'bootswatch/dist/cosmo/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div className="container">
-    <App />
-  </div>
+  <BrowserRouter>
+    <Menu />
+    <div className="container">
+      <App />
+    </div>
+  </BrowserRouter>
 );
 
---
+++ front/pro-atividade-app/src/pages/clientes/ClienteForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import TitlePage from "./../../components/TitlePage";
+import { Button } from 'react-bootstrap';
+import { useNavigate, useParams } from "react-router-dom";
+
+export default function ClienteForm() {
+	const navigate = useNavigate();
+	let { id } = useParams();
+
+    return (
+        <>
+            <TitlePage title={"Cliente Deatalhes " + (id !== undefined ? id : "")}>
+				<Button variant='outline-secondary' onClick={() => (navigate('/cliente/lista'))}>
+                    <i className="fas fa-arrow-left me-2"></i>
+                    Voltar
+                </Button>
+			</TitlePage>
+            <div>ahahah</div>
+        </>
+    );
+}
